ddbab97ff4df8a6233486283afc33fa21acbd5f3
Leo Meyerovich <leo@graphistry.com>
2021-11-05 02:18:19 -0700

    fix(docs): missing theme

---
+++ projects/client-api-react/.storybook/theme.js
@@ -0,0 +1,10 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Graphistry JS docs',
+  brandUrl: 'https://www.graphistry.com',
+  brandImage: 'https://hub.graphistry.com/static/assets/images/logo/banner_transparent_colored.png',
+});
