85380b93c6f03b8b55801f03611365ed99212664
Leo Meyerovich <leo@graphistry.com>
2022-05-21 14:53:51 -0700

    docs(demo): cra18

---
+++ projects/cra-test-18/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { Graphistry } from '@graphistry/client-api-react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Testing Graphistry</h1>
+        <Graphistry dataset={'Miserables'}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
