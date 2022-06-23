b2a73368783750c8b2f9ddef34c45804a2de0199
Desirree Adegunle <87389186+dess890@users.noreply.github.com>
2022-06-23 15:38:32 -0400

    fix(Client.ts): stale check for client added

--- projects/js-upload-api/src/Client.ts
+++ projects/js-upload-api/src/Client.ts
@@ -1,4 +1,3 @@
-import  fetch  from 'node-fetch';
 /**
  * # Client examples
  * 
@@ -61,6 +60,33 @@ export class Client {
 
     private _token?: string;
     private _version?: string;
+    private _getAuthTokenPromise?: Promise<string>; // undefined if not configured
+
+    private fetch: any;
+
+    checkStale(username: string, password: string, protocol: string, host: string, clientProtocolHostname?: string): boolean {
+        if (this.username !== username) {
+            console.debug('username changed', {currentUsername: this.username, newUsername: username}, this);
+            return true;
+        }
+        if (this._password !== password) {
+            console.debug('password changed', {currentPassword: this._password, newPassword: password}, this);
+            return true;
+        }
+        if (this.protocol !== protocol) {
+            console.debug('protocol changed', {currentProtocol: this.protocol, newProtocol: protocol}, this);
+            return true;
+        }
+        if (this.host !== host) {
+            console.debug('host changed', {currentHost: this.host, newHost: host}, this);
+            return true;
+        }
+        if (this.clientProtocolHostname !== clientProtocolHostname) {
+            console.debug('clientProtocolHostname changed', {currentClientProtocolHostname: this.clientProtocolHostname, newClientProtocolHostname: clientProtocolHostname}, this);
+            return true;
+        }
+        return false;
+    }
 
     /**
      * @param username The username to authenticate with.
@@ -80,8 +106,11 @@ export class Client {
         this._password = password;
         this.protocol = protocol;
         this.host = host;
+        this.fetch = fetch;
         this.clientProtocolHostname = clientProtocolHostname || `${protocol}://${host}`;
-        this.getAuthToken();
+        if (this.isServerConfigured()) {
+            this._getAuthTokenPromise = this.getAuthToken();
+        }
     }
 
     /**
@@ -92,8 +121,11 @@ export class Client {
      * @returns The response from the server. 
      */
     public async post(uri: string, payload: any): Promise<any> {
+        console.debug('post', {uri, payload});
         const headers = await this.getSecureHeaders();
+        console.debug('post', {headers});
         const response = await this.postToApi(uri, payload, headers);
+        console.debug('post response', {uri, payload, response});
         return response;
     }
 
@@ -109,13 +141,31 @@ export class Client {
 
      public isServerConfigured(): boolean {
         console.debug('isServerConfigured', {username: this.username, _password: this._password, host: this.host});
-        return this.username !== '' && this._password !== '' && this.host !== '';
+        return (this.username || '') !== '' && (this._password || '') !== '' && (this.host || '') !== '';
+    }
+
+    public static isConfigurationValid(username: string, password: string, host: string): boolean {
+        console.debug('isConfigurationValid', {username: username, password: password, host: host});
+        return (username || '') !== '' && (password || '') !== '' && (host || '') !== '';
     }
 
     private async getAuthToken(force = false): Promise<string> {
         if (!force && this.authTokenValid()) {
             return this._token || '';  // workaround ts not recognizing that _token is set
         }
+
+        //Throw exception if invalid username or password
+        if (!this.isServerConfigured()) {
+            console.debug('current config', {username: this.username, _password: this._password, host: this.host});
+            throw new Error('Invalid username or password');
+        }
+
+        if (!force && this._getAuthTokenPromise) {
+            console.debug('reusing outstanding auth promise');
+            return await this._getAuthTokenPromise;
+        }
+
+        console.debug('getAuthToken', {username: this.username, _password: this._password, host: this.host});
 
         const response = await this.postToApi(
             'api/v2/auth/token/generate',
@@ -134,11 +184,14 @@ export class Client {
     }
 
     private async postToApi(url: string, data: any, headers: any): Promise<any> {
-        const response = await fetch(this.getBaseUrl() + url, { // change this
+        const resolvedFetch = this.fetch;
+        console.debug('postToApi', {url, data, headers});
+        const response = await resolvedFetch(this.getBaseUrl() + url, { // change this
             method: 'POST',
             headers,
             body: JSON.stringify(data),
         })
+        console.debug('postToApi', {url, data, headers, response});
         return await response.json();
     }
 
@@ -152,7 +205,9 @@ export class Client {
     private async getSecureHeaders(): Promise<any> {
         const headers = this.getBaseHeaders();
         const tok = await this.getAuthToken();
-        headers.append('Authorization', `Bearer ${tok}`);
+        console.debug('getSecureHeaders', {headers, tok});
+        headers.Authorization = `Bearer ${tok}`;
+        console.debug('getSecureHeaders', {headers});
         return headers;
     }
 
