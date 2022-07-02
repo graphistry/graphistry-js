b90fae6ccbce88e3b21e3cae74391f7c1a241406
Leo Meyerovich <leo@graphistry.com>
2022-07-02 11:40:23 -0700

    fix(packaging): js-upload-api

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,7 +11,7 @@ See [Conventional Commits](https://conventionalcommits.org) for commit guideline
 
 # Latest
 
-## 4.1.6 - 2022-07-02
+## 4.1.7 - 2022-07-02
 
 ### Fix
 
--- tools/extract-dist.sh
+++ tools/extract-dist.sh
@@ -6,14 +6,15 @@ IMAGE_NAME=${IMAGE_NAME:-graphistry/graphistry-js}
 DIST_CLIENT_API=${DIST_CLIENT_API:-projects/client-api/dist}
 DIST_CLIENT_API_REACT=${DIST_CLIENT_API_REACT:-projects/client-api-react/dist}
 DIST_NODE_API=${DIST_NODE_API:-projects/node-api/dist}
+DIST_JS_UPLOAD_API=${DIST_JS_UPLOAD_API:-projects/js-upload-api/dist}
 
 echo "===================="
 echo "="
 echo "=  extract-dist.sh"
 echo "="
 echo "===================="
-echo "Extracting JS binaries from graphistry/graphistry-js:latest into path projects/{client,node}-api[-react]/dist"
-echo " (DIST_CLIENT_API, DIST_CLIENT_API_REACT, DIST_NODE_API)"
+echo "Extracting JS binaries from graphistry/graphistry-js:latest into path projects/{client,node,js-upload}-api[-react]/dist"
+echo " (DIST_CLIENT_API, DIST_CLIENT_API_REACT, DIST_NODE_API, DIST_JS_UPLOAD_API)"
 echo
 echo "---------------"
 echo
@@ -23,6 +24,7 @@ echo "  IMAGE_NAME: $IMAGE_NAME"
 echo "  DIST_CLIENT_API: $DIST_CLIENT_API"
 echo "  DIST_CLIENT_API_REACT: $DIST_CLIENT_API_REACT"
 echo "  DIST_NODE_API: $DIST_NODE_API"
+echo "  DIST_JS_UPLOAD_API: $DIST_JS_UPLOAD_API"
 echo
 echo "---------------"
 echo
@@ -37,6 +39,7 @@ echo "2. Copy distro files from container '$CONTAINER_NAME' to path '$DIST_PATH'
 docker cp graphistry-js-tmp:/opt/graphistry-js/projects/client-api/dist ${DIST_CLIENT_API}
 docker cp graphistry-js-tmp:/opt/graphistry-js/projects/client-api-react/dist ${DIST_CLIENT_API_REACT}
 docker cp graphistry-js-tmp:/opt/graphistry-js/projects/node-api/dist ${DIST_NODE_API}
+docker cp graphistry-js-tmp:/opt/graphistry-js/projects/js-upload-api/dist ${DIST_JS_UPLOAD_API}
 
 echo "3. Clean up container '$CONTAINER_NAME"
 docker rm -f graphistry-js-tmp
@@ -52,7 +55,7 @@ echo
 echo "---------------"
 echo
 echo "[[ Generated JS binaries ]]"
-du -ah $DIST_PATH | grep "client-api/dist\|client-api-react/dist|node-api/dist"
+du -ah $DIST_PATH | grep "client-api/dist\|client-api-react/dist\|node-api/dist\|js-upload-api/dist"
 echo
 echo "Successfully exiting with return code 0"
 exit 0
