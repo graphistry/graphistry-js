2644b300695c0e68f97c9f9e9742cc7ff91ff125
Leo Meyerovich <leo@graphistry.com>
2022-09-09 14:14:31 -0700

    fix(publishing): extract proper paths and include js-upload-api

--- tools/extract-dist.sh
+++ tools/extract-dist.sh
@@ -3,9 +3,10 @@ set -e
 
 CONTAINER_NAME=${CONTAINER_NAME:-graphistry-js-tmp}
 IMAGE_NAME=${IMAGE_NAME:-graphistry/graphistry-js}
-DIST_CLIENT_API=${DIST_CLIENT_API:-projects/client-api/dist}
-DIST_CLIENT_API_REACT=${DIST_CLIENT_API_REACT:-projects/client-api-react/dist}
-DIST_NODE_API=${DIST_NODE_API:-projects/node-api/dist}
+DIST_CLIENT_API=${DIST_CLIENT_API:-projects/client-api}
+DIST_CLIENT_API_REACT=${DIST_CLIENT_API_REACT:-projects/client-api-react}
+DIST_NODE_API=${DIST_NODE_API:-projects/node-api}
+DIST_JS_UPLOAD_API=${DIST_JS_UPLOAD_API:-projects/js-upload-api}
 
 echo "===================="
 echo "="
@@ -23,23 +24,28 @@ echo "  IMAGE_NAME: $IMAGE_NAME"
 echo "  DIST_CLIENT_API: $DIST_CLIENT_API"
 echo "  DIST_CLIENT_API_REACT: $DIST_CLIENT_API_REACT"
 echo "  DIST_NODE_API: $DIST_NODE_API"
+echo "  DIST_JS_UPLOAD_API: $DIST_JS_UPLOAD_API"
 echo
 echo "---------------"
 echo
 
-echo "1. Create container $CONTAINER_NAME from image $IMAGE_NAME if needed"
-if [ ! "$(docker ps -a | grep $CONTAINER_NAME)" ]; then
-    echo "... No container '$CONTAINER_NAME', create"    
-    docker create -it --name "$CONTAINER_NAME" "$IMAGE_NAME" bash
+echo "1. Recreate container $CONTAINER_NAME from image $IMAGE_NAME as needed"
+if [ "$(docker ps -a | grep $CONTAINER_NAME)" ]; then
+    #echo "... No container '$CONTAINER_NAME', create"
+    echo "Removing contaier"
+    docker rm --name "$CONTAINER_NAME"
 fi
+echo "Creating container"
+docker create -it --name "$CONTAINER_NAME" "$IMAGE_NAME" bash    
 
 echo "2. Copy distro files from container '$CONTAINER_NAME' to path '$DIST_PATH'"
 docker cp graphistry-js-tmp:/opt/graphistry-js/projects/client-api/dist ${DIST_CLIENT_API}
 docker cp graphistry-js-tmp:/opt/graphistry-js/projects/client-api-react/dist ${DIST_CLIENT_API_REACT}
 docker cp graphistry-js-tmp:/opt/graphistry-js/projects/node-api/dist ${DIST_NODE_API}
+docker cp graphistry-js-tmp:/opt/graphistry-js/projects/js-upload-api/dist ${DIST_JS_UPLOAD_API}
 
 echo "3. Clean up container '$CONTAINER_NAME"
-docker rm -f graphistry-js-tmp
+docker rm -f $CONTAINER_NAME
 
 echo
 echo "==============="
