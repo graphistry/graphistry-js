e05354833c900fd14c7635d9f893149c96c06bd0
Alex Warren <exrhizo@gmail.com>
2022-12-03 13:05:51 -0800

    fix(cra-test): Update demo with style improvements

--- projects/cra-test/src/App.js
+++ projects/cra-test/src/App.js
@@ -7,13 +7,21 @@ import { Client, Graphistry } from '@graphistry/client-api-react';
 console.debug('app', { Graphistry, React });
 
 function App() {
+  const onSubscription = (obs, sub) => {
+    console.debug('sub', { obs, sub });
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Testing Graphistry</h1>
-        <Graphistry dataset={'Miserables'} />
+        Embeded Graphistry
       </header>
+      <Graphistry 
+        containerClassName='Content'
+        dataset={'Miserables'}
+        iframeStyle={{ height: '100%', width: '100%', border: 0 }}
+        onSubscription={onSubscription}
+      />
     </div>
   );
 }
