6b52a2abef3854d1ab020cf8c85d569ad85bceb6
Manfred Cheung <mj3cheun@gmail.com>
2024-03-11 11:01:09 -0400

    add node-api pkey export

--- projects/node-api/src/index.ts
+++ projects/node-api/src/index.ts
@@ -1,4 +1,4 @@
-import { Client as ClientBase } from '@graphistry/js-upload-api';
+import { Client as ClientBase, ClientPkey as ClientPkeyBase } from '@graphistry/js-upload-api';
 import { version as VERSION } from './version.js';
 import fetch from 'node-fetch-commonjs';
 
@@ -44,6 +44,46 @@ export class Client extends ClientBase {
         console.debug('new client', { username }, { password }, { protocol }, { host }, { clientProtocolHostname }, { version });
         super(
             username, password, org,
+            protocol, host, clientProtocolHostname,
+            fetch,
+            version,
+            '@graphistry/node-api');
+    }
+}
+
+/**
+ * Class wrapping @graphistry/js-upload-api::ClientPkey for client->server File and Dataset uploads using personal key authentication.
+ * @global
+ * @extends ClientPkeyBase
+ */
+export class ClientPkey extends ClientPkeyBase {
+    /**
+     * Create a Client
+     * @constructor
+     * @param {string} personalKeyId - Graphistry server personal key ID
+     * @param {string} personalKeySecret - Graphistry server personal key secret
+     * @param {string} org - Graphistry organization (optional)
+     * @param {string} [protocol='https'] - 'http' or 'https' for client->server upload communication
+     * @param {string} [host='hub.graphistry.com'] - Graphistry server hostname
+     * @param {clientProtocolHostname} clientProtocolHostname - Override URL base path shown in browsers. By default uses protocol/host combo, e.g., https://hub.graphistry.com
+     * 
+     * For more examples, see @graphistry/node-api and @graphistry/js-upload-api docs
+     * 
+     * @example **Authenticate against Graphistry Hub**
+     * ```javascript
+     * import { Client } from '@graphistry/client-api';
+     * const client = new Client('my_personal_key_id', 'my_personal_key_secret');
+     * ```
+    */
+    constructor(
+        personalKeyId: string, personalKeySecret: string, org?: string,
+        protocol = 'https', host = 'hub.graphistry.com',
+        clientProtocolHostname?: string,
+        version: string = VERSION
+    ) {
+        console.debug('new client', { personalKeyId }, { personalKeySecret }, { protocol }, { host }, { clientProtocolHostname }, { version });
+        super(
+            personalKeyId, personalKeySecret, org,
             protocol, host, clientProtocolHostname,
             fetch,
             version,
